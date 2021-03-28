02014b05b5654fb0c7c93896d6ea38b4f65cbd20
Anthony Bobsin <bobsinj@gmail.com>
2021-03-28 16:01:32 -0400

    setup plugin script

---
+++ packages/docusaurus-protobuffet-plugin/src/index.ts
@@ -0,0 +1,31 @@
+import { Plugin, LoadContext } from "@docusaurus/types"
+
+export interface PluginOptions {
+  fileDescriptorsPath: string
+}
+
+export type LoadedContent = never
+
+export function validateOptions({ options, validate }: { options: PluginOptions, validate: () => void }) {
+  if (options.fileDescriptorsPath === undefined) {
+    throw new Error("Options missing fileDescriptorsPath.");
+  }
+
+  return options;
+};
+
+export default function myPlugin(
+  context: LoadContext,
+  options: PluginOptions,
+): Plugin<LoadedContent> {
+  return {
+    name: "docusaurus-protobuffet-plugin",
+
+    extendCli(cli) {
+      cli
+        .command("dothing")
+        .description("Does something")
+        .action(() => {})
+    },
+  }
+}
