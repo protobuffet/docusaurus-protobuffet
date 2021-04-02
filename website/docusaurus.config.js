fe53b4ff7b80e4652d4008fecc27c0dc6b329b0d
Anthony Bobsin <bobsinj@gmail.com>
2021-04-02 16:30:02 -0400

    re-create website with properly formatted docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,127 +1,128 @@
-"use strict";
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-    title: 'My Protobuf Documentation',
-    tagline: 'The tagline of my Protobuf documentation',
-    url: 'https://your-docusaurus-test-site.com',
-    baseUrl: '/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'facebook',
-    projectName: 'docusaurus',
-    themeConfig: {
-        navbar: {
-            title: 'My Protobuf Documentation',
-            logo: {
-                alt: 'My Site Logo',
-                src: 'img/logo.svg',
-            },
-            items: [
-                {
-                    to: 'protodocs/Booking.proto',
-                    label: 'Protodocs',
-                    position: 'left',
-                },
-                {
-                    to: 'docs/',
-                    activeBasePath: 'docs',
-                    label: 'Docs',
-                    position: 'left',
-                },
-                { to: 'blog', label: 'Blog', position: 'left' },
-                {
-                    href: 'https://github.com/AnthonyBobsin/docusaurus-protobuffet',
-                    label: 'GitHub',
-                    position: 'right',
-                },
-            ],
+  title: 'My Protobuf Documentation',
+  tagline: 'The tagline of my Protobuf documentation',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'My Protobuf Documentation',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'protodocs/Booking.proto',
+          label: 'Protodocs',
+          position: 'left',
         },
-        footer: {
-            style: 'dark',
-            links: [
-                {
-                    title: 'Docs',
-                    items: [
-                        {
-                            label: 'Getting Started',
-                            to: 'docs/',
-                        },
-                    ],
-                },
-                {
-                    title: 'Community',
-                    items: [
-                        {
-                            label: 'Stack Overflow',
-                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                        },
-                        {
-                            label: 'Discord',
-                            href: 'https://discordapp.com/invite/docusaurus',
-                        },
-                        {
-                            label: 'Twitter',
-                            href: 'https://twitter.com/docusaurus',
-                        },
-                    ],
-                },
-                {
-                    title: 'More',
-                    items: [
-                        {
-                            label: 'Blog',
-                            to: 'blog',
-                        },
-                        {
-                            label: 'GitHub',
-                            href: 'https://github.com/AnthonyBobsin/docusaurus-protobuffet',
-                        },
-                    ],
-                },
-            ],
-            copyright: "Copyright \u00A9 " + new Date().getFullYear() + " My Protobuf Documentation, Inc. Built with Docusaurus.",
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
         },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/AnthonyBobsin/docusaurus-protobuffet',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
     },
-    presets: [
-        [
-            '@docusaurus/preset-classic',
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
             {
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
-                    editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
+              label: 'Getting Started',
+              to: 'docs/',
             },
-        ],
-        [
-            'docusaurus-protobuffet',
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
             {
-                protobuffet: {
-                    fileDescriptorsPath: './fixtures/proto_workspace.json',
-                    protoDocsPath: './protodocs',
-                    sidebarPath: './sidebarsProtodocs.js',
-                }
-            }
-        ]
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/AnthonyBobsin/docusaurus-protobuffet',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Protobuf Documentation, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
     ],
-    plugins: [
-        [
-            '@easyops-cn/docusaurus-search-local',
-            {
-                hashed: true,
-                docsRouteBasePath: 'protodocs',
-                docsDir: 'protodocs',
-                indexBlog: false,
-            },
-        ],
+    [
+      'docusaurus-protobuffet',
+      {
+        protobuffet: {
+          fileDescriptorsPath: './fixtures/proto_workspace.json',
+          protoDocsPath: './protodocs',
+          sidebarPath: './sidebarsProtodocs.js',
+        }
+      }
     ]
+  ],
+  plugins: [
+    [
+      '@easyops-cn/docusaurus-search-local',
+      {
+        hashed: true,
+        docsRouteBasePath: 'protodocs',
+        docsDir: 'protodocs',
+        indexBlog: false,
+      },
+    ],
+  ]
 };
