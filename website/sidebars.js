b97d2d531e826592fa3c98520d48ebdba30f7553
Anthony Bobsin <bobsinj@gmail.com>
2021-04-02 18:29:54 -0400

    add foundation for website docs

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,6 +2,38 @@ module.exports = {
   docs: [
     {
       type: 'category',
+      label: 'What is Protobuffet?',
+      items: [
+        'what/overview',
+        'what/docusaurus',
+        'what/protobuf',
+        'what/features',
+        'what/users',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Why use it?',
+      items: [
+        'why/documentation-reqs',
+        'why/problems-today',
+        'why/state-of-protobuf',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'How do we use it?',
+      items: [
+        'how/packages',
+        'how/demo',
+        'how/installation',
+        'how/usage',
+        'how/docusaurus',
+        'how/advanced'
+      ],
+    },
+    {
+      type: 'category',
       label: 'Docusaurus Tutorial',
       items: [
         'getting-started',
