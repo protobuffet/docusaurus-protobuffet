20466d4195a765ae75c35aea82cc12ef5a578e7f
Jan Kukuczka <jan.kukuczka@gmail.com>
2023-07-02 11:06:30 +0200

    Added option `templatePath` to use custom template for generated MDX

--- packages/docusaurus-protobuffet-plugin/src/generators/docfile.ts
+++ packages/docusaurus-protobuffet-plugin/src/generators/docfile.ts
@@ -1,102 +1,23 @@
-import { Enum, FileDescriptor, FileDescriptors, GeneratedDocFile, Message, Service } from '../types';
-import { getLeafFileName } from '../utils';
+import { FileDescriptors, GeneratedDocFile } from "../types";
+import { getLeafFileName } from "../utils";
+import { compile } from "handlebars";
+import defaultTemplate from "./template";
 
-export const generateDocFiles = (fileDescriptors: FileDescriptors): GeneratedDocFile[] => {
-  const { files }  = fileDescriptors;
-  return files.map(generateDocFile);
+export const generateDocFiles = (
+  fileDescriptors: FileDescriptors,
+  template?: string
+): GeneratedDocFile[] => {
+  const helpers: { [name: string]: Function } = {
+    stringify: JSON.stringify,
+    getLeafFileName,
+  };
+
+  const compiledTemplate = compile(template ?? defaultTemplate);
+
+  const { files } = fileDescriptors;
+  return files.map((fileDescriptor) => ({
+    fileContents: compiledTemplate(fileDescriptor, { helpers }),
+    fileName: fileDescriptor.name,
+    fileDescriptor,
+  }));
 };
-
-const generateDocFile = (fileDescriptor: FileDescriptor): GeneratedDocFile => ({
-  fileContents: generateDocFileContents(fileDescriptor),
-  fileName: fileDescriptor.name,
-  fileDescriptor,
-});
-
-const generateDocFileContents = (fileDescriptor: FileDescriptor): string => {
-  // TODO: run through prettier for consistent formatting.
-  return (
-  `---
-title: ${getLeafFileName(fileDescriptor.name)}
-hide_title: true
----
-
-import { ProtoMessage, ProtoServiceMethod, ProtoEnum } from '@theme/ProtoFile';
-
-# \`${getLeafFileName(fileDescriptor.name)}\`
-_**path** ${fileDescriptor.name}_
-
-_**package** ${fileDescriptor.package}_
-
-${fileDescriptor.description}
-
----
-
-${
-  [
-    generateMessageSectionMdx(fileDescriptor.messages),
-    generateEnumSectionMdx(fileDescriptor.enums),
-    generateServiceSectionMdx(fileDescriptor.services),
-  ].filter(Boolean).map(section => section + "\n---\n").join("")
-}
-
-  `);
-};
-
-const generateMessageSectionMdx = (messages: Message[]): string|null => {
-  if (messages.length == 0) {
-    return null;
-  }
-
-  return (
-    `## Messages
-
-${messages.map((message, i) => (
-`
-### \`${message.longName}\`
-<ProtoMessage key={${i}} message={${JSON.stringify(message)}} />
-`
-)).join("\n")}`
-  );
-};
-
-const generateEnumSectionMdx = (enums: Enum[]): string|null => {
-  if (enums.length == 0) {
-    return null;
-  }
-
-  return (
-    `## Enums
-
-${enums.map((enumb, i) => (
-`
-### \`${enumb.longName}\`
-<ProtoEnum key={${i}} enumb={${JSON.stringify(enumb)}} />
-`
-)).join("\n")}`
-  );
-}
-
-const generateServiceSectionMdx = (services: Service[]): string|null => {
-  if (services.length == 0) {
-    return null;
-  }
-
-  return (
-    `## Services
-
-${services.map((service, i) => (
-`
-### \`${service.name}\`
-
-${service.description}
-
-${service.methods.map((method, i) => (
-`
-#### \`${method.name}\`
-<ProtoServiceMethod key={'${method.name}-${i}'} method={${JSON.stringify(method)}} />
-`
-)).join("\n")}
-`
-)).join("\n")}`
-  );
-}
--- packages/docusaurus-protobuffet-plugin/src/index.ts
+++ packages/docusaurus-protobuffet-plugin/src/index.ts
@@ -1,45 +1,72 @@
-import { Plugin, LoadContext } from "@docusaurus/types"
-import { existsSync, lstatSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
-import path from 'path';
+import { Plugin, LoadContext } from "@docusaurus/types";
+import {
+  existsSync,
+  lstatSync,
+  mkdirSync,
+  readFileSync,
+  writeFileSync,
+} from "fs";
+import path from "path";
 
-import { generateDocFiles, generateSidebarFileContents } from './generators';
-import { parseFileDescriptors } from './parsers';
+import { generateDocFiles, generateSidebarFileContents } from "./generators";
+import { parseFileDescriptors } from "./parsers";
 
 export interface PluginOptions {
   // Path to Protobuf file descriptors JSON file. See: https://protobuffet.com/docs/how/usage#generating-the-filedescriptorspath-file
-  fileDescriptorsPath: string
+  fileDescriptorsPath: string;
   // Path to generate data on filesystem relative to site dir.
   protoDocsPath: string;
   // Path to sidebar configuration for showing a list of markdown pages.
   sidebarPath: string;
   // URL route for the docs section of your site.
   routeBasePath: string;
+  // Optional: Path to the custom document template file. The template uses the Handlebars format.
+  templatePath?: string;
 }
 
-export function validateOptions({ options, validate }: { options: PluginOptions, validate: () => void }): PluginOptions {
-  const { fileDescriptorsPath, protoDocsPath, sidebarPath } = options;
+export function validateOptions({
+  options,
+  validate,
+}: {
+  options: PluginOptions;
+  validate: () => void;
+}): PluginOptions {
+  const { fileDescriptorsPath, protoDocsPath, sidebarPath, templatePath } =
+    options;
 
   // fileDescriptorsPath is an existing json file
   if (!fileDescriptorsPath || !existsSync(fileDescriptorsPath)) {
-    throw new Error('Expected fileDescriptorsPath option to reference a present file.');
+    throw new Error(
+      "Expected fileDescriptorsPath option to reference a present file."
+    );
   }
 
   // protoDocsPath is a directory. we only check if it's a directory if it already exists.
-  if (!protoDocsPath || (existsSync(protoDocsPath) && !lstatSync(protoDocsPath).isDirectory())) {
-    throw new Error('Expected protoDocsPath option to reference a directory.');
+  if (
+    !protoDocsPath ||
+    (existsSync(protoDocsPath) && !lstatSync(protoDocsPath).isDirectory())
+  ) {
+    throw new Error("Expected protoDocsPath option to reference a directory.");
   }
 
   // sidebarPath is a present file
   if (!sidebarPath) {
-    throw new Error('Expected sidebarPath option to reference a file.');
+    throw new Error("Expected sidebarPath option to reference a file.");
+  }
+
+  // templatePath is optional, but when set, check if the file exists
+  if (templatePath && !existsSync(templatePath)) {
+    throw new Error(
+      `Could not find the template file "${templatePath}" referenced in option templatePath.`
+    );
   }
 
   return options;
-};
+}
 
 export default function plugin(
   context: LoadContext,
-  options: PluginOptions,
+  options: PluginOptions
 ): Plugin<never> {
   return {
     name: "docusaurus-protobuffet-plugin",
@@ -50,14 +77,24 @@ export default function plugin(
         .description("Generate documentation for a protobuf workspace.")
         .action(() => {
           // read file descriptors JSON file
-          const fileDescriptorsInput = JSON.parse(readFileSync(options.fileDescriptorsPath).toString());
-          const fileDescriptors = parseFileDescriptors(fileDescriptorsInput, options.routeBasePath);
+          const fileDescriptorsInput = JSON.parse(
+            readFileSync(options.fileDescriptorsPath).toString()
+          );
+          const fileDescriptors = parseFileDescriptors(
+            fileDescriptorsInput,
+            options.routeBasePath
+          );
+
+          // read a document template file
+          const template = options.templatePath
+            ? readFileSync(options.templatePath).toString()
+            : undefined;
 
           // generate markdown files for each in fileDescriptors
-          const docFiles = generateDocFiles(fileDescriptors);
+          const docFiles = generateDocFiles(fileDescriptors, template);
 
           // write files to appropriate directories
-          docFiles.forEach(docFile => {
+          docFiles.forEach((docFile) => {
             const fileName = `${options.protoDocsPath}/${docFile.fileName}.mdx`;
             const fileDir = path.dirname(fileName);
 
@@ -73,11 +110,11 @@ export default function plugin(
 
           // write sidebar object
           writeFileSync(options.sidebarPath, sidebarFileContents);
-        })
+        });
     },
 
     getThemePath() {
       return path.resolve(__dirname, "./theme");
-    }
-  }
+    },
+  };
 }
